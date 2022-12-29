dbb6b8f6e024ce90862a1ae848300f5003c0dfbd
guga tabatadze <gugatabatadze@gmail.com>
2022-12-29 15:42:33 +0400

    first commit

---
+++ JwtWebApi/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtWebApi
+{
+    public class UserDto
+    {
+/*DTO-data transfer object*/
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
